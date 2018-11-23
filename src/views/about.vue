6aba6080da1fd8d2eafc0308cce101e9d33bf6ff
wang926454 <1107224733@qq.com>
2018-11-23 16:20:51 +0800

    更新关于页面的弹幕

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container">
     <div class="item" v-for="(item, index) in list" :key="index" @click="openLink(item.link)">
-      <vue-danmaku class="item-wrap" :ref="'danmaku-' + item.id" :config="item.config || config" :danmus="item.danmus">
-        
-      </vue-danmaku>
+      <vue-danmaku class="item-wrap" :ref="'danmaku-wang926454'" :config="config" :danmus="item.danmus"></vue-danmaku>
     </div>
   </div>
 </template>
@@ -19,35 +17,40 @@ export default {
       list: [
         {
           id: 'wang926454',
-          name: '随心',
-          more: '第一第一',
-          more2: '',
           link: 'https://me.wang64.cn',
           danmus: [
             '看什么看，不就是一堆弹幕啊~',
-            '⊙▂⊙',
-            '轻轻点击打开关于页面~',
-            '⊙ω⊙',
-            '●▂●',
-            '⊙﹏⊙',
-            '∩﹏∩',
-            '我猜你肯定盯着看我写的弹幕了~',
             "(●'◡'●)",
-            '⊙０⊙',
-            'hhh~',
-            '⊙︿⊙',
-            '∩０∩',
-            '●ω●',
-            '∪ω∪',
-            '≧ω≦',
-            '＞ω＜',
-            '∩ω∩',
+            '轻轻点击打开关于页面~∩ω∩',
+            '看到这条弹幕你已经不是前1000了',
+            '暂停听BGM',
+            '大军还有一秒到达现场~⊙﹏⊙',
+            '我猜你肯定盯着看我写的弹幕了',
+            "红红火火恍恍惚惚，韩红会画画后悔画韩寒~(●'◡'●)",
+            '开心的像个200多斤的孩子~⊙０⊙',
+            '前方高能，非战斗人员赶快撤离',
+            '散了吧，要X的~⊙︿⊙',
+            'B站看片指日可待~∩０∩',
+            '此生无悔入海贼，来世愿做船上人',
+            '我觉得，我可以再抢救一下~●ω●',
+            'BGM~爱的自杀，再问供养，唉，我都要哭了~∪ω∪',
+            '干了这碗入江陈醋，来世还做傲娇直树~≧ω≦',
+            '卡在了奇怪的地方~＞ω＜',
+            '刘星的蚂蚁花被',
+            '胖儿子沉迷游戏，秃父苦劝无言',
+            '233333',
+            '6666666',
+            '瞬间爆炸',
+            '朱军画质',
+            '牛顿的棺材板压不住了',
+            '教练我想学这个',
             '实在是不知道说什么~o(*////▽////*)o'
           ]
         }
       ],
       config: {
         channels: 6,
+        speed: 7,
         loop: true
       }
     }
@@ -55,15 +58,12 @@ export default {
   computed: {},
   created () { },
   mounted () {
-    this.$refs['danmaku-' + 'wang926454'][0].play()
+    this.$refs['danmaku-wang926454'][0].play()
     setTimeout(() => {
-      this.$refs['danmaku-' + 'wang926454'][0].resize()
+      this.$refs['danmaku-wang926454'][0].resize()
     }, 300)
   },
   methods: {
-    onMouseOut (id) {
-      this.$refs['danmaku-' + id][0].stop()
-    },
     openLink (link) {
       window.open(link)
     }
@@ -101,30 +101,6 @@ export default {
       display: flex;
       align-items: center;
       justify-content: center;
-    }
-    .avatar {
-      float: left;
-      width: 70px;
-      height: 70px;
-      margin: 0 10px;
-      img {
-        height: 60px;
-        max-width: 60px;
-        border-radius: 50%;
-      }
-    }
-    .intro {
-      h2 {
-        font-size: 18px;
-        color: #eee;
-      }
-      p {
-        font-size: 14px;
-        color: #ccc;
-      }
-      span {
-        opacity: 0;
-      }
     }
   }
 }
